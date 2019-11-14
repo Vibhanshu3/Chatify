4923496d1eea65a0a0025bd5cad7dc1880d2f546
PSK <puneet.katoch@gmail.com>
2019-11-15 00:15:36 +0530

    profile view

---
+++ app/src/main/java/ViewAnimation.java
@@ -0,0 +1,17 @@
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.view.View;
+
+public class ViewAnimation {
+    public static boolean rotateFab(final View v, boolean rotate) {
+        v.animate().setDuration(200)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                    }
+                })
+                .rotation(rotate ? 135f : 0f);
+        return rotate;
+    }
+}
